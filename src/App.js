f4c735c30ae67d70f2931ff900af590d3a734894
dev-turan <devturan007@gmail.com>
2024-01-03 18:48:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo} from "./utils/HandleApi";
 function App() {
+
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* App Name */}
+      <div className="appname">
+        <h1>ＴａｓｋＦｌｏｗ</h1>
+      </div>
+      
+      <div className="container">
+        {/* heading */}
+        <h2>Organize Your Day</h2>
+
+        <div className="top">
+          {/* user input */}
+          <div className="inputclass">
+            <input
+              type="text"
+              placeholder="Add your task here."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          {/* button */}
+          <div 
+           className="add" 
+           onClick={ isUpdating ?
+             () => updateToDo (toDoId, text, setToDo, setText, setIsUpdating)
+             : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? "Update" : "Add"}
+          </div>
+        </div>
+
+        {/* ToDo List */}
+        <div className="list">
+
+          {toDo.map((item) => <ToDo
+           key={item._id} 
+           text={item.text} 
+           updateMode={() => updateMode(item._id, item.text)}
+           deleteToDo={() => deleteToDo(item._id, setToDo)}
+           />)}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
